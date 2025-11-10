5f1a72e136c39de2e98146785cdee5fab09c643e
andres-sanabria-10 <andres.sanabria02@uptc.edu.co>
2025-11-10 09:37:42 -0500

    5000 price

--- app/dashboard/feligres/solicitud-partida/page.tsx
+++ app/dashboard/feligres/solicitud-partida/page.tsx
@@ -39,7 +39,7 @@ const partidaTypes = [
     description: "Solicita una copia de tu partida de Bautismo.",
     type: "Baptism",
     icon: BookOpen,
-    price: 4000,
+    price: 5000,
   },
   {
     title: "Confirmación",
